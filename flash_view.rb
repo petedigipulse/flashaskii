e0e54d3b0d3168206970b3847f7553933004317f
Cara Hill <chilly23@gmail.com>
2015-04-03 12:34:00 +1300

    More methods for view.

--- flash_view.rb
+++ flash_view.rb
@@ -1,7 +1,28 @@
 class FlashAsciiView
 
-  def render(anything)
-    puts anything
+  def welcome_message
+    puts "Welcome to Flashascii, A Game For Flash Asses.")
+  end
+
+  def display_question
+    puts "Question: \n\n"
+  end
+
+  def ask_for_guess
+    puts "Your guess:\n\n"
+    gets.chomp
+  end
+
+  def correct_guess
+    puts "Way to go! Check out this flash Ascii art: /n/n"
+  end
+
+  def incorrect_guess
+    puts "Boo, you suck!"
+  end
+
+  def finish_message
+    puts "Thanks for being a Flash Ass."
   end
 
 end
