a36c48638fa223b41d36be21d7f19d022989df8e
Alex Revell <apprentice@StMac8.local>
2015-04-03 16:20:15 +1300

    working but art out of phase

--- flash_controller.rb
+++ flash_controller.rb
@@ -1,28 +1,28 @@
 require_relative ('./flash_model_deck')
 require_relative ('./flash_view')
+require_relative ('./ascii_model')
 
 class FlashController
-
 
 	def initialize
 		@view = FlashAsciiView.new
 		@deck = FlashCardDeck.new
-		#@art = FlashArt.new
-
+		@ascii = Ascii.new
 	end
-
 
 	def start
 		@view.welcome_message
 		until @deck.flashcards.empty?
 			correct = false
 			current_card = @deck.next_card
-			until correct 
+			until correct
 				@view.display_question(current_card.question)
 				user_guess = @view.ask_for_guess
 				if user_guess == current_card.answer
-					@view.correct_guess 
-					#display ascii
+					@view.correct_guess
+					art = @ascii.next_ascii
+					@ascii.parse_file(art)
+					@ascii.print_ascii
 					correct = true
 				else
 					@view.incorrect_guess
