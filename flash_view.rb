a6aed8634debb5ebd2123b2f38d65a950476abca
Cara Hill <chilly23@gmail.com>
2015-04-03 13:53:58 +1300

    A couple more changes.

--- flash_view.rb
+++ flash_view.rb
@@ -1,15 +1,15 @@
 class FlashAsciiView
 
   def welcome_message
-    puts "Welcome to Flashascii, A Game For Flash Asses.")
+    puts "Welcome to Flashascii, A Game For Flash Asses."
   end
 
-  def display_question
-    puts "Question: \n\n"
+  def display_question(question)
+    puts "Question:\n\n #{question}"
   end
 
   def ask_for_guess
-    puts "Your guess:\n\n"
+    puts "Your guess: "
     gets.chomp
   end
 
@@ -21,7 +21,7 @@ class FlashAsciiView
     puts "Boo, you suck!"
   end
 
-  def finish_message
+  def final_message
     puts "Thanks for being a Flash Ass."
   end
 
